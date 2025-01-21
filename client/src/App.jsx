35919d881767b5a83cf6949227c09418f751c847
Ajit Behl <Abehl3036@conestogac.on.ca>
2025-01-21 10:00:00 -0500

    feat: set up project environment and create Auth page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Button } from "./components/ui/button";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Auth from "./pages/auth/auth.jsx";
+import Chat from "./pages/chat/chat";
+import Profile from "./pages/profile/Profile";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+    <Routes>
+      <Route path ='/auth' element={<Auth/>}/>
+      <Route path ='/chat' element={<Chat/>}/>
+      <Route path ='/profile' element={<Profile/>}/>
+      <Route path = '*' element= {<Navigate to= "/auth"/>}/>
+    </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
