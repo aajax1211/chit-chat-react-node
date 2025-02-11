fdc12b9b35391208548b449e742de38b0e083d44
Ajit Behl <Abehl3036@conestogac.on.ca>
2025-02-11 10:24:44 -0500

    Added endpoint for channel communication to retrieve all contacts except the user

--- server/routes/ContactRoutes.js
+++ server/routes/ContactRoutes.js
@@ -1,9 +1,10 @@
 import {Router} from "express";
 import { verifyToken } from "../middlewares/AuthMiddleware.js";
-import { getContactsForDMList, searchContacts } from "../controllers/ContactsController.js";
+import { getAllContacts, getContactsForDMList, searchContacts } from "../controllers/ContactsController.js";
 
 
 export const contactRoutes = Router();
 
 contactRoutes.post("/search", verifyToken,searchContacts)
 contactRoutes.get("/get-contacts-for-dm", verifyToken, getContactsForDMList)
+contactRoutes.get("/get-all-contacts", verifyToken, getAllContacts)
